b8d1548a2c207fe1def03180bdcd3ab19517b6f9
Eduardo Y. Woo kim <edudukim.ed@gmail.com>
2025-01-14 17:20:02 -0300

    ParallaxBackground

--- scripts/bg-script/parallax-bg.gd
+++ scripts/bg-script/parallax-bg.gd
@@ -1,3 +1,15 @@
 extends ParallaxBackground
 class_name Background
 
+export(Array,int) var spd_layers
+export(bool) var can_process
+
+func _ready():
+	
+	if can_process == false:
+		set_physics_process(false)
+		
+func _physics_process(delta : float):
+	for index in get_child_count():
+		if get_child(index) is ParallaxLayer:
+			get_child(index).motion_offset.x -= delta * spd_layers[index]
